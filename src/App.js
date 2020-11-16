c54cd92df571d32382831b7ae2748093c308ea5d
IGOR <igornesan@gmail.com>
2020-11-16 10:51:01 +0200

    first init and create store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+//Redux setting
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
